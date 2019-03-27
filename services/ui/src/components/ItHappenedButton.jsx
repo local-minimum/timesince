4652767f4ba55a437d17e0ad4810fcbde8266129
Martin Zackrisson <martin.zackrisson@fripost.org>
2019-03-27 19:13:28 +0100

    It happened button

--- services/ui/src/components/ItHappenedButton.jsx
+++ services/ui/src/components/ItHappenedButton.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import './ItHappenedButton.css';
 
+const SUMBIT_STEP = 100;
+
 export default class ItHappenedButton extends Component {
   constructor(props) {
     super(props);
@@ -9,38 +11,65 @@ export default class ItHappenedButton extends Component {
     this.onMouseDown = this.onMouseDown.bind(this);
     this.onMouseUp = this.onMouseUp.bind(this);
     this.onCountProgress = this.onCountProgress.bind(this);
+    this.clearForget = this.clearForget.bind(this);
   }
 
-  onMouseDown() {
-    let { submitProgress } = this.state;
+  onMouseDown(evt) {
+    let { submitProgress} = this.state;
     if (submitProgress == null) {
       submitProgress = 50;
-      this.setState({ submitProgress });
-      setTimeout(this.onCountProgress, 200);
+      this.setState({ submitProgress, progressTxt: '3', progressIter: 1 });
+      setTimeout(this.onCountProgress, SUMBIT_STEP);
     }
+    evt.preventDefault();
   }
 
   onCountProgress() {
-    let { submitProgress } = this.state;
+    let { submitProgress, progressIter } = this.state;
     if ( submitProgress == null ) return;
     submitProgress = Math.floor(submitProgress / 1.5);
-    this.setState({ submitProgress });
+    let { progressTxt } = this.state;
+    if ( progressIter > 12 && submitProgress === 0) {
+      progressTxt = 'Just happened';
+    } else if ( progressIter > 8) {
+      progressTxt = '1';
+    } else if (progressIter > 4) {
+      progressTxt = '2';
+    } else if (progressIter > 12) {
+      console.log(progressIter);
+      progressTxt = `e ${progressIter}`;
+    }
     if (submitProgress > 0) {
-      setTimeout(this.onCountProgress, 200);
+      setTimeout(this.onCountProgress, SUMBIT_STEP);
+      this.setState({ submitProgress, progressIter: progressIter + 1,  progressTxt });
     } else {
       const { onRegisterEvent, id } = this.props;
       onRegisterEvent(id);
+      this.setState({ progressTxt: "Just now", progressIter: null});
     }
   }
 
   onMouseUp() {
-    this.setState({ submitProgress: null });
+    const { progressIter } = this.state;
+    if (progressIter !== null) {
+      this.setState({ submitProgress: null, progressTxt: 'Forget that' });
+      setTimeout(this.clearForget, 1000);
+    } else {
+      this.setState({ submitProgress: null });
+    }
+  }
+
+  clearForget() {
+    const { progressTxt } = this.state;
+    if (progressTxt === 'Forget that') {
+      this.setState({ progressTxt: null });
+    }
   }
 
   render() {
-    const { submitProgress } = this.state;
+    const { submitProgress, progressTxt } = this.state;
     const { value, unit, id } = this.props;
-    const ButtonTxt = value == null ? 'Never happened' : `${value} ${unit} ago`;
+    const ButtonTxt = progressTxt != null ? progressTxt : (value == null ? 'Never happened' : `${value} ${unit} ago`);
     const extraStyle = submitProgress == null ? null : { boxShadow: `0px 0px ${submitProgress}px red`};
     const canSubmit = id != null;
 
